d3831f6491fdc4c22c7bd6dd711deb4b34f35cd8
Camille <camille@gmail.com>
2026-05-03 15:45:08 -0300

    atualização docker

---
+++ projeto-docker/src/main/java/com/exemplo/projeto_docker/ProjetoDockerApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.projeto_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDockerApplication.class, args);
+	}
+
+}
---
+++ projeto-docker/src/test/java/com/exemplo/projeto_docker/ProjetoDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.projeto_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
